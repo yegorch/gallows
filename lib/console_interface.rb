5c918707744a0b4baa149136317fec043a559f4d
Yegor Chsherbakov <76652963+yegorch@users.noreply.github.com>
2021-04-13 11:20:51 +0600

    Add files via upload

--- lib/console_interface.rb
+++ lib/console_interface.rb
@@ -8,13 +8,10 @@ class ConsoleInterface
   end
 
   def print_out
-    puts <<~END
-      Слово: #{word_to_show}
-      #{figure}
-      Ошибки (#{@game.errors_made}): #{errors_to_show}
-      У вас осталось ошибок: #{@game.errors_allowed}
-
-    END
+    puts "Слово:#{word_to_show}".colorize(:blue)
+    puts  figure.colorize(:yellow)
+    puts "Ошибки (#{@game.errors_made}): #{errors_to_show}".colorize(:red)
+    puts "У вас осталось ошибок: #{@game.errors_allowed}".colorize(:red)
 
     if @game.won?
       puts "Поздравляем, вы выиграли!"
